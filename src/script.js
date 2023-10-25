6d425559a0fc1d30e7e81a701f9f498fa65e0083
ron <ron.fried15@gmail.com>
2023-10-25 16:29:40 +0300

    change

--- src/script.js
+++ src/script.js
@@ -2,12 +2,12 @@
 const endpoint =
   "http://api.taboola.com/1.0/json/taboola-templates/recommendations.get";
 const params = {
+  "publisher id": "taboola-templates",
   "app.type": "desktop",
-  "app.apikey": "f9040ab1b9c802857aa783c469d0e0ff7e7366e4",
-  count: 4, // Number of recommendations to fetch
   "source.type": "video",
+  "app.apikey": "f9040ab1b9c802857aa783c469d0e0ff7e7366e4",
   "source.id": "214321562187",
-  "source.url": "http://www.site.com/videos/214321562187.html",
+  count: 4, // Number of recommendations to fetch
 };
 
 // Construct the URL with query parameters
